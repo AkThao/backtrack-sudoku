b920cb0527c8e5171ba3f2b1d982919f00e83209
AkThao <akaash.thao@gmail.com>
2020-06-14 19:07:04 +0100

    Clean up code

--- solve_3x3.py
+++ solve_3x3.py
@@ -82,24 +82,21 @@ def find_empty_cells(board):
 def check_cell_value(board, cell, test_value):
     """Return True if the given number is legal
         i.e. does not appear in the current row or column
-    Return False if the given number is illegal"""
-    # print(board[cell[0],:], board[:,cell[1]])
+    Return False if the given number is illegal
+    """
     if test_value not in board[cell[0],:] and test_value not in board[:,cell[1]]:
-        # print(f"Value: {test_value} - True")
         return True
-    # print(f"Value: {test_value} - False")
     return False
 
 
 def update_cell(board, cell):
-    """Try the available numbers from the current cell value + 1
-    Return the board if the current cell was successfully updated
-    Otherwise, if all available numbers are illegal, return False"""
-    # print(f"\nCurrent cell: {cell}")
-    # print(f"Board: {board}")
+    """Try to update the current cell
+    Return a two-tuple, with second element as the board
+    First element is True if the cell was successfully updated
+    First element is False otherwise
+    """
     cell_value = board[cell[0],cell[1]]
     cell_value_index = available_nums.index(cell_value)
-    # print(f"Cell value: {cell_value}")
     if cell_value_index == len(available_nums) - 1:
         board[cell[0],cell[1]] = 0
         return (False, board)
@@ -107,7 +104,6 @@ def update_cell(board, cell):
     for num in available_nums[cell_value_index + 1:]:
         if check_cell_value(board, cell, num):
             board[cell[0],cell[1]] = num
-            # print(board)
             return (True, board)
         elif available_nums.index(num) == len(available_nums) - 1:
             board[cell[0],cell[1]] = 0
@@ -115,25 +111,24 @@ def update_cell(board, cell):
 
 
 def solve(board, empty_cells):
+    """Perform the backtrack algorithm"""
     count = 0
     while count != len(empty_cells):
-        # print(count)
         result = update_cell(board, empty_cells[count])
-        if result[0] == False:
+        if result[0] == False:  # Cell was not updated, so backtrack
             count -= 1
-        else:
+        else:  # Cell was updated, so carry on to the next cell
             count += 1
-
 
     return result[1]
 
 
 def main():
     for b in BOARDS:
-        board = np.array(b)  # make a copy of the original board
+        board = np.array(b)  # Make a copy of the original board
         empty_cells = find_empty_cells(board)
         board = solve(board, empty_cells)
-        print(board)
+        print(board)  # Solved puzzle
 
 
 if __name__ == "__main__":
@@ -141,4 +136,3 @@ if __name__ == "__main__":
 
 
 # Notes:
-# Fix issues with multiple backtracks
